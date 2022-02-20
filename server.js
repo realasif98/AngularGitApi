e55779e92ef3c2f89994dc35162c7c9d44ef0585
Mohammad Asif <mohdasifsp21@gmail.com>
2022-02-20 11:01:47 +0530

    change dir

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static('src/assets'));
+app.use(express.static(__dirname + '/src/assets'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join('src/index.html'));   
+    res.sendFile(path.join(__dirname+'/src/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 4000, () => {
-    console.log("Node app is running at localhost:");
+    console.log("Node app is running at localhost: 4000");
 });
