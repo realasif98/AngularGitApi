2212f8241de7b7deebb71220c513ef5cf8096f00
Mohammad Asif <mohdasifsp21@gmail.com>
2022-02-20 11:21:57 +0530

    ch

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const app = express();
 // Serve only the static files form the dist directory    
 app.use(express.static(__dirname + '/src/assets'));
 
-app.get('/', function(req,res) {  
-    console.log(path.join(__dirname+'/src/index.html'));
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/angular-git-api/', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/angular-git-api/', 'index.html'));
+}); 
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 4000, () => {
