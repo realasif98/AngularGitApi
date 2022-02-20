ff9cd171de7b28945f2fcd2775cccbd52f68f142
Mohammad Asif <mohdasifsp21@gmail.com>
2022-02-20 11:06:06 +0530

    path changes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 // Serve only the static files form the dist directory    
 app.use(express.static(__dirname + '/src/assets'));
 
-app.get('/*', function(req,res) {  
+app.get('/', function(req,res) {  
+    console.log(path.join(__dirname+'/src/index.html'));
     res.sendFile(path.join(__dirname+'/src/index.html'));   
 });  
 
