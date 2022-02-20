6b183b5f12796cc5a4a94208fd9262d8661a7a37
Mohammad Asif <mohdasifsp21@gmail.com>
2022-02-20 10:55:54 +0530

    new chnages

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ app.get('/*', function(req,res) {
 });  
 
 // Start the app by listening on the default Heroku port    
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Node app is running at localhost:");
+});
