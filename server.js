7e149c5f24e9fa12661196fd2839c12f4e3de4c7
Mohammad Asif <mohdasifsp21@gmail.com>
2022-02-20 11:13:55 +0530

    chnages

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/src/assets'));
 
 app.get('/', function(req,res) {  
     console.log(path.join(__dirname+'/src/index.html'));
-    res.sendFile(path.join(__dirname+'/src/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
